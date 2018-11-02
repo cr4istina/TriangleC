a1d361669640d953bdcb036c1e0679338fbd2cad
cr4istina <ave.anit@gmail.com>
2018-11-02 16:00:56 +0200

    console inpt project

---
+++ InputController.java
@@ -0,0 +1,18 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class InputController {
+
+    public static Triangle createTriangle() {
+        System.out.println("Please, enter 3 numbers to be the sides of your triangle, here:");
+        double a = new Scanner(System.in).nextDouble();
+        double b = new Scanner(System.in).nextDouble();
+        double c = new Scanner(System.in).nextDouble();
+        System.out.println("The sides of the triangle are: ");
+        Triangle triangle  = new Triangle(a, b, c);
+
+        return triangle;
+    }
+
+
+}
---
+++ Triangle.java
@@ -0,0 +1,13 @@
+public class Triangle {
+    public double side1;
+    public double side2;
+    public double side3;
+
+
+    public Triangle(double side1, double side2, double side3){
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+        System.out.println(side1 + " " + side2 + " " + side3);
+    }
+}
